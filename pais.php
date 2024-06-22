e8f33d10c3d2000a44f90f72ed102426313d7f10
Gabriel Rodrigues Rolim <100931450+rolim265@users.noreply.github.com>
2024-06-21 21:07:11 -0300

    Add files via upload
    Atualização PHP e JS, paginas 'pais.php' , 'index.html' , BD 'mapamundi.sql' [FELLIPE] , necessário realizar algumas adaptações no front-end por conta do back, que infelizmente teve alguns conflitos com o front, porém foram minimos, tudo funcional (NA HORA DE COLOCAR OS NOMES NO BANCO, NAO PODE TER ERRO DE ORTOGRAFIA, SE TIVER ELE NAO VAI ABRIR)

---
+++ pais.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informações do país</title>
+    <link rel="stylesheet" href="assets/css/pais.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+        <a href="index.html" class="button">RETORNAR AO MAPA</a>
+        <div id="slide-container">
+            <div id="slide">
+                <?php
+                $conexao = mysqli_connect("localhost", "root", "", "mapamundi");
+                if (!$conexao) {
+                    die("Erro na conexão: " . mysqli_connect_error());
+                }
+                $paisId = isset($_GET['id']) ? mysqli_real_escape_string($conexao, $_GET['id']) : '';
+                $consulta = "SELECT * FROM paises WHERE nome = '$paisId'";
+                $resultado = mysqli_query($conexao, $consulta);
+                if (mysqli_num_rows($resultado) > 0) {
+                    while ($registro = mysqli_fetch_assoc($resultado)) {
+                        echo "
+                            <div class='content'>
+                                <img class='flag' src='assets/img/{$registro['nome']}.png' alt='{$registro['nome']}' srcset=''>
+                                <div class='name'>{$registro['nome']}</div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/presidente.png);'>
+                                <div class='infos'>
+                                    <p>PRESIDENTE</p>
+                                    <p>{$registro['presidente']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/capital.png);'>
+                                <div class='infos'>
+                                    <p>CAPITAL</p>
+                                    <p>{$registro['capital']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/moeda.png);'>
+                                <div class='infos'>
+                                    <p>MOEDA</p>
+                                    <p>{$registro['moeda']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/idioma.png);'>
+                                <div class='infos'>
+                                    <p>IDIOMA</p>
+                                    <p>{$registro['idioma']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/pib.png);'>
+                                <div class='infos'>
+                                    <p>PIB</p>
+                                    <p>{$registro['pib']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/idh.png);'>
+                                <div class='infos'>
+                                    <p>IDH</p>
+                                    <p>{$registro['idh']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/populacao.png);'>
+                                <div class='infos'>
+                                    <p>POPULAÇÃO</p>
+                                    <p>{$registro['populacao']}</p>
+                                </div>
+                            </div>
+                            <div class='item' style='background-image: url(assets/img/territorio.png);'>
+                                <div class='infos'>
+                                    <p>TERRITÓRIO</p>
+                                    <p>{$registro['territorio']}</p>
+                                </div>
+                            </div>";
+                    }
+                } else {
+                    echo "<p>Nenhum registro encontrado.</p>";
+                }
+                mysqli_close($conexao);
+                ?>
+            </div>
+        </div>
+        <div class="buttons">
+            <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
+            <button id="next"><i class="fa-solid fa-angle-right"></i></button>
+        </div>
+    </div>
+
+    <script src="assets/js/pais.js"></script>
+</body>
+</html>
